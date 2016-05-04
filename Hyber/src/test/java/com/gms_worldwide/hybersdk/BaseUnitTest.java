a3886e6c99d552ecc0cd3046126e3c96a1d3fcc8
Andrew Kochura <a.kochura@gms-worlwide.com>
2016-05-04 17:24:08 +0300

    Global changing the structure of the project.

--- Hyber/src/test/java/com/gms_worldwide/hybersdk/BaseUnitTest.java
+++ Hyber/src/test/java/com/gms_worldwide/hybersdk/BaseUnitTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class BaseUnitTest {
     /**
      * Addition is correct.
      *
